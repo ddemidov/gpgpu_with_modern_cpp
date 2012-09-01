f527a12c5c98c35000d391838431fd4b5094a2fc
Denis Demidov <ddemidov@ksu.ru>
2012-09-01 17:40:23 +0400

    Lorenz ensemble data updated

--- data/lorenz_ensemble/process_results.m
+++ data/lorenz_ensemble/process_results.m
@@ -1,45 +1,67 @@
 close all
 clear all
 
-thrust = load('thrust_gpu.dat');
-vexcl  = load('vexcl_1gpu.dat');
-vienna = load('viennacl_gpu.dat');
-n  = unique(thrust(:,1))';
+test = {'thrust_gpu', 'thrust_cpu', ...
+	'viennacl_gpu', 'viennacl_cpu', ...
+	'vexcl_1gpu', 'vexcl_2gpu', 'vexcl_3gpu', 'vexcl_cpu'};
 
-thrust_avg = [];
-vexcl_avg  = [];
-vienna_avg = [];
+lgnd = {'Thrust GPU', 'Thrust CPU', ...
+	'ViennaCL GPU', 'ViennaCL CPU', ...
+	'VexCL 1 GPU', 'VexCL 2 GPU', 'VexCL 3 GPU', 'VexCL CPU'};
 
-for i = n
-    I = find(thrust(:,1) == i);
-    time = sum(thrust(I,2)) / length(I);
-    thrust_avg = [thrust_avg time];
-
-    I = find(vexcl(:,1) == i);
-    time = sum(vexcl(I,2)) / length(I);
-    vexcl_avg = [vexcl_avg time];
-
-    I = find(vienna(:,1) == i);
-    time = sum(vienna(I,2)) / length(I);
-    vienna_avg = [vienna_avg time];
-end
-
+style = {'ko-', 'kd-', ...
+	 'bo-', 'bd-', ...
+	 'ro-', 'rs-', 'rv-', 'rd-'};
 
 figure(1)
-set(gca, 'FontSize', 18)
+set(gcf, 'position', [50, 50, 1000, 500]);
 
+subplot(1, 2, 1);
+set(gca, 'FontSize', 10);
+subplot(1, 2, 2);
+set(gca, 'FontSize', 10);
 
-loglog(n, thrust_avg, 'ko-', 'linewidth', 1, 'markersize', 6, 'markerfacecolor', 'w');
-hold on
-loglog(n, vexcl_avg,  'ro-', 'linewidth', 1, 'markersize', 6, 'markerfacecolor', 'w');
-loglog(n, vienna_avg, 'bo-', 'linewidth', 1, 'markersize', 6, 'markerfacecolor', 'w');
+idx = 0;
+for t = test
+    idx = idx + 1;
+    data = load([cell2mat(t) '.dat']);
+    avg = [];
 
+    n = unique(data(:,1))';
+    for i = n
+	I = find(data(:,1) == i);
+	time = sum(data(I,2)) / length(I);
+	avg = [avg time];
+    end
+
+    if idx == 1
+	ref_avg = avg;
+    end
+
+    subplot(1, 2, 1);
+    loglog(n, avg, style{idx}, 'markersize', 4, 'markerfacecolor', 'w');
+    hold on
+
+    subplot(1, 2, 2);
+    loglog(n, avg ./ ref_avg, style{idx}, 'markersize', 4, 'markerfacecolor', 'w');
+    hold on
+end
+
+subplot(1, 2, 1);
 xlim([1e2 1e7])
+ylim([1e-1 1e5])
 set(gca, 'xtick', [1e2 1e3 1e4 1e5 1e6 1e7])
 xlabel('N');
 ylabel('T (sec)');
-
-legend('thrust gpu', 'vexcl 1 gpu', 'viennacl gpu', 'location', 'northwest');
+legend(lgnd, 'location', 'NorthWest');
 legend boxoff
+axis square
 
-print('-depsc', 'abs.eps');
+subplot(1, 2, 2);
+xlim([1e2 1e7])
+set(gca, 'xtick', [1e2 1e3 1e4 1e5 1e6 1e7])
+xlabel('N');
+ylabel('T / T(Thrust GPU)');
+axis square
+
+print('-depsc', 'lorenz_ensemble.eps');
