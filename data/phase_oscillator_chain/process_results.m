b7f22d5586c03b57e4420bddffaacc3692bd35aa
Denis Demidov <ddemidov@ksu.ru>
2012-09-02 10:26:38 +0400

    Phase oscillator data updated

--- data/phase_oscillator_chain/process_results.m
+++ data/phase_oscillator_chain/process_results.m
@@ -1,112 +1,64 @@
 close all
 clear all
 
-t  = load('thrust_gpu.dat');
-tc = load('thrust_cpu.dat');
-v1 = load('vexcl_1gpu.dat');
-v2 = load('vexcl_2gpu.dat');
-v3 = load('vexcl_3gpu.dat');
-vc = load('vexcl_cpu.dat');
-n  = unique(t(:,1))';
+test = {'thrust_gpu', 'thrust_cpu', ...
+	'vexcl_1gpu', 'vexcl_2gpu', 'vexcl_3gpu', 'vexcl_cpu'};
 
-tavg  = [];
-tcavg = [];
-v1avg = [];
-v2avg = [];
-v3avg = [];
-vcavg = [];
+lgnd = {'Thrust GPU', 'Thrust CPU', ...
+	'VexCL 1 GPU', 'VexCL 2 GPU', 'VexCL 3 GPU', 'VexCL CPU'};
 
-for i = n
-    I = find(t(:,1) == i);
-    time = sum(t(I,2)) / length(I);
-    tavg = [tavg time];
-
-    I = find(tc(:,1) == i);
-    time = sum(tc(I,2)) / length(I);
-    tcavg = [tcavg time];
-
-    I = find(v1(:,1) == i);
-    time = sum(v1(I,2)) / length(I);
-    v1avg = [v1avg time];
-
-    I = find(v2(:,1) == i);
-    time = sum(v2(I,2)) / length(I);
-    v2avg = [v2avg time];
-
-    I = find(v3(:,1) == i);
-    time = sum(v3(I,2)) / length(I);
-    v3avg = [v3avg time];
-
-    I = find(vc(:,1) == i);
-    time = sum(vc(I,2)) / length(I);
-    vcavg = [vcavg time];
-end
-
+style = {'ko-', 'kd-', ...
+	 'ro-', 'rs-', 'rv-', 'rd-'};
 
 figure(1)
-set(gca, 'FontSize', 18)
+set(gcf, 'position', [50, 50, 1000, 500]);
 
-loglog(n, tcavg, 'kd-', ...
-		'linewidth', 2, 'markersize', 6, 'markerfacecolor', 'w');
+subplot(1, 2, 1);
+set(gca, 'FontSize', 10);
+subplot(1, 2, 2);
+set(gca, 'FontSize', 10);
 
-hold on
+idx = 0;
+for t = test
+    idx = idx + 1;
+    data = load([cell2mat(t) '.dat']);
+    avg = [];
 
-loglog(n, tavg, 'ko-', ...
-		'linewidth', 2, 'markersize', 6, 'markerfacecolor', 'w');
+    n = unique(data(:,1))';
+    for i = n
+	I = find(data(:,1) == i);
+	time = sum(data(I,2)) / length(I);
+	avg = [avg time];
+    end
 
-loglog(n, vcavg, 'md-', ...
-		'linewidth', 2, 'markersize', 6, 'markerfacecolor', 'w');
+    if idx == 1
+	ref_avg = avg;
+    end
 
-loglog(n, v1avg, 'ro-', ...
-		'linewidth', 2, 'markersize', 6, 'markerfacecolor', 'w');
+    subplot(1, 2, 1);
+    loglog(n, avg, style{idx}, 'markersize', 4, 'markerfacecolor', 'w');
+    hold on
 
-loglog(n, v2avg, 'bo-', ...
-		'linewidth', 2, 'markersize', 6, 'markerfacecolor', 'w');
+    subplot(1, 2, 2);
+    loglog(n, avg ./ ref_avg, style{idx}, 'markersize', 4, 'markerfacecolor', 'w');
+    hold on
+end
 
-loglog(n, v3avg, 'go-', ...
-		'linewidth', 2, 'markersize', 6, 'markerfacecolor', 'w');
-
+subplot(1, 2, 1);
 xlim([1e2 1e7])
+%ylim([1e-1 1e5])
 set(gca, 'xtick', [1e2 1e3 1e4 1e5 1e6 1e7])
 xlabel('N');
 ylabel('T (sec)');
-
-legend('thrust cpu', 'thrust gpu', 'vexcl cpu', 'vexcl 1 gpu', 'vexcl 2 gpu', 'vexcl 3 gpu', ...
-    'location', 'northwest');
+legend(lgnd, 'location', 'NorthWest');
 legend boxoff
+axis square
 
-print('-depsc', 'abs.eps');
-
-figure(2)
-set(gca, 'FontSize', 18)
-
-loglog(n, tcavg ./ tavg, 'kd-', ...
-		'linewidth', 2, 'markersize', 6, 'markerfacecolor', 'w');
-
-hold on
-
-loglog(n, vcavg ./ tavg, 'md-', ...
-		'linewidth', 2, 'markersize', 6, 'markerfacecolor', 'w');
-
-loglog(n, v1avg ./ tavg, 'ro-', ...
-		'linewidth', 2, 'markersize', 6, 'markerfacecolor', 'w');
-
-loglog(n, v2avg ./ tavg, 'bo-', ...
-		'linewidth', 2, 'markersize', 6, 'markerfacecolor', 'w');
-
-loglog(n, v3avg ./ tavg, 'go-', ...
-		'linewidth', 2, 'markersize', 6, 'markerfacecolor', 'w');
-
-loglog(n, ones(size(n)), 'k:');
-
-h = legend('thrust cpu', 'vexcl cpu', 'vexcl 1 gpu', 'vexcl 2 gpu', 'vexcl 3 gpu');
-legend boxoff
-set(h, 'FontSize', 16)
-
+subplot(1, 2, 2);
 xlim([1e2 1e7])
-ylim([1e-1 1e3])
 set(gca, 'xtick', [1e2 1e3 1e4 1e5 1e6 1e7])
 xlabel('N');
-ylabel('T / T(thrust gpu)');
+ylabel('T / T(Thrust GPU)');
+axis square
 
-print('-depsc', 'rel.eps');
+print('-depsc', 'phase_oscillator.eps');
