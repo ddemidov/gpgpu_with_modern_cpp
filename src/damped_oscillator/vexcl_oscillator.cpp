8609be945aed0bbfed6581a0036d5879b7d0855d
Denis Demidov <ddemidov@ksu.ru>
2012-09-27 12:04:53 +0400

    Replaced custom kernels in vexcl with multioperations

--- src/damped_oscillator/vexcl_oscillator.cpp
+++ src/damped_oscillator/vexcl_oscillator.cpp
@@ -25,65 +25,21 @@ struct oscillator
     value_type m_offset;
     value_type m_omega_d;
 
-    std::vector<cl::Kernel> step;
-
     oscillator(const std::vector<cl::CommandQueue> &queue,
 	    value_type omega, value_type amp, value_type offset, value_type omega_d
 	    )
         : m_omega( omega ) , m_amp( amp ) , m_offset( offset ) , m_omega_d( omega_d )
     {
-	static const char source[] =
-	    "#if defined(cl_khr_fp64)\n"
-	    "#  pragma OPENCL EXTENSION cl_khr_fp64: enable\n"
-	    "#elif defined(cl_amd_fp64)\n"
-	    "#  pragma OPENCL EXTENSION cl_amd_fp64: enable\n"
-	    "#endif\n"
-	    "kernel void oscillator(\n"
-	    "        ulong n,\n"
-	    "        global double *dx,\n"
-	    "        global double *dy,\n"
-	    "        global const double *x,\n"
-	    "        global const double *y,\n"
-	    "        double omega,\n"
-	    "        double eps\n"
-	    "        )\n"
-	    "{\n"
-	    "    size_t i = get_global_id(0);\n"
-	    "    if (i < n) {\n"
-	    "        double X = x[i];\n"
-	    "        double Y = y[i];\n"
-	    "        dx[i] = eps * X + omega * Y;\n"
-	    "        dy[i] = eps * Y - omega * X;\n"
-	    "    }\n"
-	    "}\n";
-	    
-	step.reserve(queue.size());
-	for(auto &q : queue) {
-	    cl::Context context = q.getInfo<CL_QUEUE_CONTEXT>();
-	    cl::Program program = vex::build_sources(context, source);
-	    step.emplace_back(program, "oscillator");
-	}
     }
 
     void operator()( const state_type &x , state_type &dxdt , value_type t )
     {
-	auto &queue = x(0).queue_list();
-
         value_type eps = m_offset + m_amp * cos( m_omega_d * t );
 
-	for(uint d = 0; d < queue.size(); d++) {
-	    if (size_t psize = x(0).part_size(d)) {
-		step[d].setArg(0, psize);
-		step[d].setArg(1, dxdt(0)(d));
-		step[d].setArg(2, dxdt(1)(d));
-		step[d].setArg(3, x(0)(d));
-		step[d].setArg(4, x(1)(d));
-		step[d].setArg(5, m_omega);
-		step[d].setArg(6, eps);
-
-		queue[d].enqueueNDRangeKernel(step[d], cl::NullRange, psize, cl::NullRange);
-	    }
-	}
+	dxdt = std::make_tuple(
+		eps * x(0) + m_omega * x(1),
+		eps * x(1) - m_omega * x(0)
+		);
     }
 };
 
