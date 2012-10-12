b10e4dc3f67ccb982e6112323ff04e825dad6410
Denis Demidov <ddemidov@ksu.ru>
2012-10-12 13:45:31 +0400

    Updated job submission scripts

--- src/run_tests.sh
+++ src/run_tests.sh
@@ -4,8 +4,8 @@ for exp in damped_oscillator disordered_ham_lattice \
     lorenz_ensemble phase_oscillator_chain
 do
     pushd $exp
-    for job in *.sge; do
-	qsub $job
+    for job in cpu 1gpu 2gpu 3gpu; do
+	qsub run_on_${job}.sge
     done
     popd
 done
