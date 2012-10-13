6f66a3b9e7004043f8dda1604aaca1f0ad85ee30
Denis Demidov <ddemidov@ksu.ru>
2012-10-13 10:19:15 +0400

    Updated plots for VexCL scaling

---
+++ data/lorenz_ensemble/experiment.m
@@ -0,0 +1,72 @@
+classdef experiment
+
+    properties (GetAccess=public)
+	fname
+	legend
+	style
+	fcolor
+	msize
+	lwidth
+
+	n
+	t
+    end
+
+    methods
+	function obj = experiment(fname, legend, style, fcolor, msize, lwidth)
+	    obj.legend = legend;
+	    obj.style  = style;
+	    obj.fcolor = fcolor;
+
+	    if nargin >= 5
+		obj.msize  = msize;
+	    else
+		obj.msize = 5;
+	    end
+
+	    if nargin >= 6
+		obj.lwidth = lwidth;
+	    else
+		obj.lwidth = 1;
+	    end
+
+	    data = load([fname '.dat']);
+
+	    obj.n = unique(data(:,1))';
+	    obj.t = [];
+	    for i = obj.n
+		I = find(data(:,1) == i);
+		time = median(data(I,2));
+		obj.t = [obj.t time];
+	    end
+	end
+
+	function loglog(obj, data)
+	    if nargin >= 2
+		loglog(obj.n, data, obj.style,     ...
+		    'MarkerSize',      obj.msize,  ...
+		    'MarkerFaceColor', obj.fcolor, ...
+		    'LineWidth',       obj.lwidth);
+	    else
+		loglog(obj.n, obj.t, obj.style,    ...
+		    'MarkerSize',      obj.msize,  ...
+		    'MarkerFaceColor', obj.fcolor, ...
+		    'LineWidth',       obj.lwidth);
+	    end
+	end
+
+	function semilogx(obj, data)
+	    if nargin >= 2
+		semilogx(obj.n, data, obj.style,   ...
+		    'MarkerSize',      obj.msize,  ...
+		    'MarkerFaceColor', obj.fcolor, ...
+		    'LineWidth',       obj.lwidth);
+	    else
+		semilogx(obj.n, obj.t, obj.style,  ...
+		    'MarkerSize',      obj.msize,  ...
+		    'MarkerFaceColor', obj.fcolor, ...
+		    'LineWidth',       obj.lwidth);
+	    end
+	end
+    end
+end
--- data/lorenz_ensemble/vexcl_scaling.m
+++ data/lorenz_ensemble/vexcl_scaling.m
@@ -1,41 +1,37 @@
 close all
 clear all
 
-test   = {'vexcl_1gpu', 'vexcl_2gpu', 'vexcl_3gpu'};
-lgnd   = {'Tesla  \times 1', 'Tesla  \times 2', 'Tesla  \times 3'};
-style  = {'k:', 'ko-', 'ko--'};
-fcolor = {'w', 'w', 'w'};
-msize  = 4;
+ms = 4;
+lw = 0.5;
+fs = 26;
+
+ex = [ ...
+    experiment('vexcl_1gpu',        'Tesla  \times 1',  'k:',  'w', ms, lw) ...
+    experiment('vexcl_2gpu',        'Tesla  \times 2',  'ko-', 'w', ms, lw) ...
+    experiment('vexcl_3gpu',        'Tesla  \times 3',  'kd-', 'w', ms, lw) ...
+    experiment('vexcl_1gpu_tahiti', 'Tahiti  \times 1', 'k:',  'k', ms, lw) ...
+    experiment('vexcl_2gpu_tahiti', 'Tahiti  \times 2', 'ko-', 'k', ms, lw) ...
+    experiment('vexcl_3gpu_tahiti', 'Tahiti  \times 3', 'kd-', 'k', ms, lw) ...
+    ];
 
 figure(1)
-set(gca, 'FontSize', 26);
+set(gca, 'FontSize', fs);
 
-idx = 0;
-for t = test
-    idx = idx + 1;
-    data = load([cell2mat(t) '.dat']);
-    avg = [];
-
-    n = unique(data(:,1))';
-    for i = n
-	I = find(data(:,1) == i);
-	time = median(data(I,2));
-	avg = [avg time];
+for i = 1:length(ex)
+    if i == 1 || i == 4
+	ref = ex(i).t;
     end
 
-    if idx == 1
-	ref_avg = avg;
-    end
-
-    semilogx(n, ref_avg ./ avg, style{idx}, 'markersize', msize, 'markerfacecolor', fcolor{idx});
+    ex(i).semilogx(ref ./ ex(i).t);
     hold on
 end
 
 xlim([1e2 1e7])
+ylim([0 3])
 set(gca, 'xtick', [1e2 1e3 1e4 1e5 1e6 1e7])
 xlabel('N');
 ylabel('T(1 GPU) / T');
-legend(lgnd, 'location', 'NorthWest');
+legend(ex(:).legend, 'location', 'NorthWest');
 legend boxoff
 axis square
 
