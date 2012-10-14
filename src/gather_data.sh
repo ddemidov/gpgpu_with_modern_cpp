ddc3ea85aef84ede7fcbd25536a3296d6e0e60c8
Denis Demidov <ddemidov@ksu.ru>
2012-10-14 10:12:04 +0400

    Updated data for disordered lattice

--- src/gather_data.sh
+++ src/gather_data.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-for mask in thrust_cpu vexcl_cpu viennacl_cpu \
+for mask in thrust_cpu vexcl_cpu_intel vexcl_cpu_amd \
+    viennacl_cpu_intel viennacl_cpu_amd \
     thrust_gpu vexcl_1gpu viennacl_gpu \
     vexcl_2gpu vexcl_3gpu
 do
