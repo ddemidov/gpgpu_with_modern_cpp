8091abbde6a15d0c96b7d8dffcacbbd843345385
Peter Gottschling <pgottsch@pgottsch-CELSIUS-W510-POWER.(none)>
2012-12-14 21:41:21 +0100

    Took out the obsolete testing and timing.

--- src/disordered_ham_lattice/gmtl_disordered_lattice.cu
+++ src/disordered_ham_lattice/gmtl_disordered_lattice.cu
@@ -112,32 +112,6 @@ int main(int argc, char* argv[])
     odeint::integrate_const(stepper, sys, X, value_type(0.0), t_max, dt);
     cudaThreadSynchronize();
     std::cout << "Integration took " << timer.elapsed() << " s\n";
-    mtl::irange rr(10);
- 
-    if (n1 == 512) {
-	mtl::dense_vector<value_type> compare_first(10), compare_second(10);
-	bool check= false;
-
-	if (t_max == 10.0) {
-	    compare_first= 0.695523,-1.06336,-0.301946,-0.923554,0.62739,3.06804,2.0448,0.0154956,2.0136,-0.775507;
-	    compare_second= -1.17204,-2.89465,-0.679947,0.520701,1.06731,-2.01476,-1.60585,0.446732,-3.20896,-0.801548;
-	    check= true;
-	} else if (t_max > 0.0999 && t_max < 0.10001) {
-	    compare_first= 2.98606,2.99274,2.98692,2.98669,2.98499,2.99569,2.99362,2.98714,2.99448,2.99035;
-	    compare_second= -0.278452,-0.145232,-0.261421,-0.26603,-0.299831,-0.0862731,-0.127468,-0.256972,-0.110283,-0.192934;
-	    check= true;
-	}
-
-	if (check) {
-	    compare_first-= X.first[rr];
-	    compare_second-= X.second[rr]; 
-
-	    if (two_norm(compare_first) > 0.01 || two_norm(compare_second) > 0.01) {
-		std::cerr << "Wrong result" << std::endl;
-		// return 1;
-	    }
-	}
-    }
 
     std::cout << X.first[0] << " " << X.second[0] << std::endl;
     return 0;
